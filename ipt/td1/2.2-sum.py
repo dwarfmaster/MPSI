2bb9b1da4363da5977c26d3a3dce9eb3868bc805
lucas8 <luc.linux@mailoo.org>
2014-09-10 08:10:06 +0200

    Added the script answer to the 2.2 question of the TD.

---
+++ ipt/td1/2.2-sum.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def somme():
+    var1 = int(input("Saisissez un premier nombre "))
+    var2 = int(input("Saisissez un deuxieme nombre "))
+    print("La somme des nombres saisis vaut ", var1+var2)
+
+somme()
+
