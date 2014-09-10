1f3fbefe5a87229e7583c5f275be93e7c7880297
lucas8 <luc.linux@mailoo.org>
2014-09-10 08:58:10 +0200

    Added the python script answering the 4.3 question in the TD1.

---
+++ ipt/td1/4.3-range.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+print(list(range(0, 6)))
+print(list(range(80, 101)))
+print(list(range(7, 15*7+1, 7)))
+print(len(list(range(2, 2015, 2))))
+print(list(range(10, -1, -1)))
+print(list(range(20, -1, -2)))
+
