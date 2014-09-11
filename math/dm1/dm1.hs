e0ad1cc1d48bf4d3ce7b96b618b8c0c6bf60d381
lucas8 <luc.linux@mailoo.org>
2014-09-11 17:18:25 +0200

    Improved the haskell implementation of the first dm.

--- math/dm1/dm1.hs
+++ math/dm1/dm1.hs
@@ -1,6 +1,6 @@
 
-addLst x = if length x == 1
-            then x !! 0
-            else addLst [ (x !! y) + (x !! (y+1)) | y <- [0..(length x - 2)] ]
+addLst :: [Integer] -> Integer
+addLst (x:[]) = x
+addLst x      = addLst [ (x !! y) + (x !! (y+1)) | y <- [0..(length x - 2)] ]
 main = print (addLst [1..100])
 
