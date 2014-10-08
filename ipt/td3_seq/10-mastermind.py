5d83d9df2dcea07f4fd8254f51f60e4feaadbc5d
lucas8 <luc.linux@mailoo.org>
2014-10-08 09:49:20 +0200

    Improved the mastermind (question 9 of TD3).

--- ipt/td3_seq/10-mastermind.py
+++ ipt/td3_seq/10-mastermind.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import random as rd;
 
 def suppi(l, i):
     return l[0:i] + l[i+1:len(l)]
@@ -36,29 +37,47 @@ def reponse(c, e):
 
     return [nn, nb]
 
+def gen_code():
+    return [rd.randint(0,5),
+            rd.randint(0,5),
+            rd.randint(0,5),
+            rd.randint(0,5)]
+
+def input_value(i):
+    nb = -1
+    while nb not in list(range(0,6)):
+        nb = int(input("Entrez la valeur " + str(i) + " : "))
+    return nb
+
+def input_code():
+    cd = []
+    for i in range(1,5):
+        cd += [input_value(i)]
+    return cd
+
 def test(c, e):
-    print(e, "->", reponse(c,e))
+    rep = reponse(c, e)
+    print(e, "->", rep)
+    return rep
 
 if __name__ == "__main__":
-    test([0,1,1,2], [3,3,3,3])
-    test([0,1,1,2], [1,3,3,3])
-    test([0,1,1,2], [3,4,5,1])
-    test([0,1,1,2], [2,3,4,5])
-    test([0,1,1,2], [1,0,3,3])
-    test([0,1,1,2], [1,0,2,3])
-    test([0,1,1,2], [1,2,0,1])
-    test([0,1,1,2], [3,4,1,5])
-    test([0,1,1,2], [3,1,4,1])
-    test([0,1,1,2], [1,3,3,2])
-    test([0,1,1,2], [2,1,0,1])
-    test([0,1,1,2], [0,3,1,4])
-    test([0,1,1,2], [2,1,1,3])
-    test([0,1,1,2], [1,3,1,2])
-    test([0,1,1,2], [2,1,1,0])
-    test([0,1,1,2], [1,1,0,2])
-    test([0,1,1,2], [0,1,2,2])
-    test([0,1,1,2], [0,1,1,1])
-    test([0,1,1,2], [1,0,2,1])
-    test([0,1,1,2], [1,1,1,1])
-    test([0,1,1,2], [0,1,1,2])
+    nb = 0
+    code = []
+    ret = [0,0]
+
+    while nb not in [1,2]:
+        nb = int(input("1 ou 2 joueurs ? "))
+    if nb == 1:
+        code = gen_code()
+    else:
+        print("Joueur 1, fermez les yeux !\nJoueur 2, entrez les valeurs.")
+        code = input_code()
+
+    print("Code obtenu. Début du jeu.")
+    nb = 0
+    while ret[0] != 4:
+        ret = test(code, input_code())
+        nb += 1
+
+    print("Bravo, vous avez gagné en", nb, "coups !")
 
