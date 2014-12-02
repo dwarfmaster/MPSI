f9df921aaea02b7905eb6638498356954b7ed479
lucas8 <luc.linux@mailoo.org>
2014-12-02 21:01:22 +0100

    Added matrix manipulation methods to gauss.py

--- ipt/gauss/gauss.py
+++ ipt/gauss/gauss.py
@@ -27,11 +27,49 @@ def output_matrix(m):
             print(m[i][j], end="  ")
         print("|")
 
+######### Matrix manipulation ########
+# Swaps the contents of the i1 and i2 lines
+def matrix_swap(m, i1, i2):
+    if len(m) == 0 or len(m) <= max(i1, i2):
+        return
+    for i in range(len(m[0])):
+        m[i1][i], m[i2][i] = m[i2][i], m[i1][i]
+
+# Appends the contents of each line of m2 to each line of m1
+def matrix_append(m1, m2):
+    if len(m1) != len(m2):
+        return
+    for i in range(len(m1)):
+        m1[i].extend(m2[i])
+
+# Add n times the contents of the l2 line to the l1 one
+def matrix_add(m, l1, n, l2):
+    for i in range(len(m[0])):
+        m[l1][i] += n*m[l2][i]
+
 ######### Main loop ##########
 if __name__ == "__main__":
-    m, b = input_matrix(4, 4)
+    n = int(input("Enter n : "))
+    p = int(input("Enter p : "))
+    print("Enter the coeffs :")
+    coeffs, b = input_matrix(p, n)
     if not b:
         print("You can't even type a correct matrix !")
         exit()
-    output_matrix(m)
+    print("Enter the second members : ")
+    seconds, b = input_matrix(1, n)
+    if not b:
+        print("You can't even type a correct matrix !")
+        exit()
+
+    output_matrix(coeffs)
+    print("Appending seconds to coeffs : ")
+    matrix_append(coeffs, seconds)
+    output_matrix(coeffs)
+    print("Swapping line 0 and 1 :")
+    matrix_swap(coeffs, 0, 1)
+    output_matrix(coeffs)
+    print("Soustracting line 1 to line 0 :")
+    matrix_add(coeffs, 1, -1, 0)
+    output_matrix(coeffs)
 
