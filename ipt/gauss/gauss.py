68d6bb0c0a1caba2d86179ea6963adc86cb6f902
lucas8 <luc.linux@mailoo.org>
2014-12-03 09:33:25 +0100

    Finished the gauss solving program.

--- ipt/gauss/gauss.py
+++ ipt/gauss/gauss.py
@@ -12,6 +12,14 @@ import sys
 def is_zero(n):
     return abs(n) < 1e-10
 
+# Returns the index of the first non-zero element
+# or len(l) if the array is full of zeros
+def first_nonzero(l):
+    for i in range(len(l)):
+        if not is_zero(l[i]):
+            return i
+    return len(l)
+
 ######### IO #########
 # Returns the matrix and a boolean indicating the success
 def input_matrix(x, y):
@@ -70,6 +78,11 @@ def matrix_add(m, l1, n, l2):
     for i in range(len(m[0])):
         m[l1][i] += n*m[l2][i]
 
+# Multiply each element of the l line by n
+def matrix_mult(m, l, n):
+    for i in range(len(m[l])):
+        m[l][i] *= n
+
 ######### Gauss transformation ########
 # Remove the x-eme element in lines ]y,end[
 # Returns false if it was already removed
@@ -91,11 +104,42 @@ def gauss_remove(m, x, y):
 
 # Triangularize the matrix with p coeffs using the
 # gauss method
+# Returns the list of the complementary vars
 def gauss_trian(m, p):
     j = 0
+    comp = []
     for i in range(p):
         if gauss_remove(m, i, j):
             j += 1
+        else:
+            comp += [i]
+    return comp
+
+# Create a base for p variables, comp being the array
+# of the complementary variables
+def gauss_base(p, comp):
+    ret = []
+    for l in range(p):
+        ret.append((len(comp) + 1) * [0.0])
+    for i in range(len(comp)):
+        ret[comp[i]][i+1] = 1.0
+    return ret
+
+# Solve a triangularized linear system, writing the solutions
+# to base, p being the number of variables.
+# Returns False if there are no solutions.
+def gauss_solve(m, base, p):
+    for i in range(len(m) - 1, -1, -1):
+        z = first_nonzero(m[i])
+        if z > p:
+            continue
+        elif z == p:
+            return False
+        base[z][0] = m[i][p]
+        for j in range(z+1, p):
+            matrix_add(base, z, -m[i][j], j)
+        matrix_mult(base, z, 1.0/m[i][z])
+    return True
 
 ######### Main loop ##########
 if __name__ == "__main__":
@@ -103,10 +147,18 @@ if __name__ == "__main__":
     if not b:
         print("Invalid file :", sys.argv[1])
         exit()
-    p = len(coeffs) - 1
+    p = len(coeffs[0]) - 1
 
     output_matrix(coeffs)
     print("Gauss-triangularize :")
-    gauss_trian(coeffs, p)
+    comp = gauss_trian(coeffs, p)
     output_matrix(coeffs)
+    print("Base : ")
+    base = gauss_base(p, comp)
+    output_matrix(base)
+    if not gauss_solve(coeffs, base, p):
+        print("No solution.")
+    else:
+        print("Solution : ")
+        output_matrix(base)
 
