4aeb4156a896bca734003f283a5da8e67bf460f5
lucas8 <luc.linux@mailoo.org>
2015-01-06 21:06:14 +0100

    Added a python script to test using gauss.py as a package.

---
+++ ipt/gauss/libtest.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import gauss;
+
+mat = [
+        [ 1, 0, 0, 1 ],
+        [ 0, 1, 0, 2 ],
+        [ 0, 0, 1, 3 ]
+      ]
+
+gauss.output_matrix(mat)
+comp = gauss.triangularize(mat, 3)
+base = gauss.base(3, comp)
+if not gauss.solve(mat, base, 3):
+    print("No solution.")
+else:
+    gauss.output_results(base)
+
