70c03da0497e073c0364463c1b9cf2d43f206f2d
lucas8 <luc.linux@mailoo.org>
2015-01-07 10:55:36 +0100

    Made gauss.py documentation more "pythonic".

--- ipt/gauss/gauss.py
+++ ipt/gauss/gauss.py
@@ -12,9 +12,10 @@ import sys
 def is_zero(n):
     return abs(n) < 1e-10
 
-# Returns the index of the first non-zero element
-# or len(l) if the array is full of zeros
 def first_nonzero(l):
+    """
+    Returns the index of the first non-zero element or len(l) if the array is full of zeros
+    """
     for i in range(len(l)):
         if not is_zero(l[i]):
             return i
@@ -24,9 +25,10 @@ def first_nonzero(l):
 # The latex file :
 latex_file = False
 
-# Open the path file and start writing a solution
-# in it.
 def latex_begin(coeffs, path):
+    """
+    Open the path file and start writing a solution in it.
+    """
     global latex_file
     latex_file = open(path, "wt")
     if not latex_file:
@@ -37,8 +39,10 @@ def latex_begin(coeffs, path):
     latex_file.write("\\\\")
     return True
 
-# End the latex presentation and close the file.
 def latex_end(sol):
+    """
+    End the latex presentation and close the file.
+    """
     global latex_file
     if not latex_file:
         return
@@ -73,8 +77,10 @@ def latex_end(sol):
     latex_file.close()
     latex_file = None
 
-# Add an intermediary step to the latex file
 def latex_step(m):
+    """
+    Add an intermediary step to the latex file
+    """
     global latex_file
     if not latex_file:
         return
@@ -82,8 +88,10 @@ def latex_step(m):
     latex_matrix(m)
     latex_file.write("\\\\\n")
 
-# Output a matrix in latex format to the latex file.
 def latex_matrix(m):
+    """
+    Output a matrix in latex format to the latex file.
+    """
     global latex_file
     if not latex_file:
         return
@@ -98,8 +106,10 @@ def latex_matrix(m):
     latex_file.write("\\end{array}\\right.")
     return None
 
-# Output a single element to the latex_file
 def latex_elem(e, i, p):
+    """
+    Output a single element to the latex_file
+    """
     global latex_file
     if is_zero(e):
         latex_file.write("& ")
@@ -117,8 +127,10 @@ def latex_elem(e, i, p):
     return True
 
 ######### IO #########
-# Print results
 def output_results(m):
+    """
+    Print results
+    """
     for i in range(len(m)):
         print("|", end="")
         print(repr(m[i][0]).rjust(6), end="")
@@ -135,8 +147,10 @@ def output_results(m):
             print(repr(m[i][j]).rjust(6), end=" ")
         print("|")
 
-# Reads a matrix from a file : returns the matrix and a boolean indicating the success
 def read_matrix(path):
+    """
+    Reads a matrix from a file : returns the matrix and a boolean indicating the success
+    """
     f = open(path, 'r')
     m = []
     if not f:
@@ -161,27 +175,35 @@ def output_matrix(m):
         print("|")
 
 ######### Matrix manipulation ########
-# Swaps the contents of the i1 and i2 lines
 def matrix_swap(m, i1, i2):
+    """
+    Swaps the contents of the i1 and i2 lines
+    """
     if len(m) == 0 or len(m) <= max(i1, i2):
         return
     for i in range(len(m[0])):
         m[i1][i], m[i2][i] = m[i2][i], m[i1][i]
 
-# Add n times the contents of the l2 line to the l1 one
 def matrix_add(m, l1, n, l2):
+    """
+    Add n times the contents of the l2 line to the l1 one
+    """
     for i in range(len(m[0])):
         m[l1][i] += n*m[l2][i]
 
-# Multiply each element of the l line by n
 def matrix_mult(m, l, n):
+    """
+    Multiply each element of the l line by n
+    """
     for i in range(len(m[l])):
         m[l][i] *= n
 
 ######### Gauss transformation ########
-# Remove the x-eme element in lines ]y,end[
-# Returns false if it was already removed
 def remove(m, x, y):
+    """
+    Remove the x-eme element in lines ]y,end[
+    Returns false if it was already removed
+    """
     piv = None
     for j in range(y, len(m)):
         if is_zero(m[j][x]):
@@ -197,10 +219,11 @@ def remove(m, x, y):
     else:
         return False
 
-# Triangularize the matrix with p coeffs using the
-# gauss method
-# Returns the list of the complementary vars
 def triangularize(m, p):
+    """
+    Triangularize the matrix with p coeffs using the gauss method
+    Returns the list of the complementary vars
+    """
     j = 0
     comp = []
     for i in range(p):
@@ -211,9 +234,10 @@ def triangularize(m, p):
             comp += [i]
     return comp
 
-# Create a base for p variables, comp being the array
-# of the complementary variables
 def base(p, comp):
+    """
+    Create a base for p variables, comp being the array of the complementary variables
+    """
     ret = []
     for l in range(p):
         ret.append((len(comp) + 1) * [0.0])
@@ -221,10 +245,11 @@ def base(p, comp):
         ret[comp[i]][i+1] = 1.0
     return ret
 
-# Solve a triangularized linear system, writing the solutions
-# to bs, p being the number of variables.
-# Returns False if there are no solutions.
 def solve(m, bs, p):
+    """
+    Solve a triangularized linear system, writing the solutions to bs, p being the number of variables.
+    Returns False if there are no solutions.
+    """
     for i in range(len(m) - 1, -1, -1):
         z = first_nonzero(m[i])
         if z > p:
