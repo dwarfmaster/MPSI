a1fa02d8a37d9fe1f479bbd65415658ea5bfce48
lucas8 <luc.linux@mailoo.org>
2015-02-04 11:22:27 +0100

    Added secant method.

--- ipt/recursive/froot/froot.hs
+++ ipt/recursive/froot/froot.hs
@@ -6,10 +6,15 @@ f2 :: Float -> Float
 f2 x = (exp (-x)) - 0.5
 
 f3 :: Float -> Float
-f3 x = (1 - (exp (-x))) - 0.1
+f3 x = x*x - 2
+
+sec :: Float -> Float -> Float -> Float -> Float
+sec x1 y1 x2 y2 = -y1 * (1/delta) + x1
+                  where delta = (y2 - y1)/(x2 - x1)
 
 root :: (Float -> Float) -> Float -> Float -> Float -> Float
-root f a b e = if (b-a) < e then m else if (f a)*(f m) <= 0 then root f a m e else root f m b e
+root f a b e = if (b-a) < e then sec a (f a) b (f b) else 
+                  if (f a)*(f m) <= 0 then root f a m e else root f m b e
                where m = (a+b)/2
 
 epsilon = 0.0001
