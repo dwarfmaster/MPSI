1c324ecbeece791e83d9c7d1ab64b7375433562c
lucas8 <luc.linux@mailoo.org>
2015-02-18 20:45:24 +0100

    Added another version of the haskell program for the dm1.

--- math/dm1/dm1.hs
+++ math/dm1/dm1.hs
@@ -1,6 +1,18 @@
 
+combine :: [a] -> (a -> a -> a) -> [a]
+combine []      _ = []
+combine (e:[])  _ = []
+combine (e1:es) f = e1 `f` e2 : combine es f
+    where e2 = head es
+
 addLst :: [Integer] -> Integer
-addLst (x:[]) = x
-addLst x      = addLst [x+y | (x,y) <- zip (tail x) (init x)]
-main = print (addLst [1..100])
+addLst (e:[]) = e
+addLst  l     = addLst $ combine l (+)
+
+addLst2 :: [Integer] -> Integer
+addLst2 (x:[]) = x
+addLst2 x      = addLst [x+y | (x,y) <- zip (tail x) (init x)]
+
+main = do putStrLn $ show $ addLst  [1..100]
+          putStrLn $ show $ addLst2 [1..100]
 
