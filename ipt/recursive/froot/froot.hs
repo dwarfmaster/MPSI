4e13cd50ba165cd444f0e8006e056be1177b7160
lucas8 <luc.linux@mailoo.org>
2015-02-25 09:01:44 +0100

    Added counters in froot.

--- ipt/recursive/froot/froot.hs
+++ ipt/recursive/froot/froot.hs
@@ -16,19 +16,31 @@ sec :: Float -> Float -> Float -> Float -> Float
 sec x1 y1 x2 y2 = -y1 * (1/delta) + x1
     where delta = (y2 - y1)/(x2 - x1)
 
-secant :: (Float -> Float) -> Float -> Float -> Float -> Float
-secant f a b e = if abs (f m) < e then m
-                 else secant f ma mb e
+secant_cn :: (Float -> Float) -> Float -> Float -> Int -> Float -> (Float, Int)
+secant_cn f a b n e
+     | abs (f m) < e = (m,n)
+     | n < maxN      = secant_cn f ma mb (n+1) e
+     | otherwise     = (a ,n)           
     where m = sec a (f a) b (f b)
           ma = if f m > 0  then a else m
           mb = if f m <= 0 then b else m
+
+secant :: (Float -> Float) -> Float -> Float -> Float -> Float
+secant f a b e = m
+    where (m,_) = secant_cn f a b 0 e
 ------------------------------------------
 
 -- Dichotomy algorithm -------------------
-root :: (Float -> Float) -> Float -> Float -> Float -> Float
-root f a b e = if (b-a) < e then sec a (f a) b (f b) else 
-                  if (f a)*(f m) <= 0 then root f a m e else root f m b e
+root_cn :: (Float -> Float) -> Float -> Float -> Int -> Float -> (Float, Int)
+root_cn f a b n e
+     | (b-a) < e = (sec a (f a) b (f b), n)
+     | n < maxN  = if (f a)*(f m) <= 0 then root_cn f a m (n+1) e else root_cn f m b (n+1) e
+     | otherwise = (a,n)
     where m = (a+b)/2
+
+root :: (Float -> Float) -> Float -> Float -> Float -> Float
+root f a b e = m
+    where (m,_) = root_cn f a b 0 e
 ------------------------------------------
 
 -- Newton method -------------------------
@@ -36,16 +48,24 @@ delta :: (Float -> Float) -> Float -> Float
 delta f x = ((f (x+h)) - (f (x-h))) / (2*h)
     where h = 1e-5
 
-newton :: (Float -> Float) -> (Float -> Float) -> Float -> Float -> Float
-newton f df xn e = if xn - xn1 < e then xn1 else newton f df xn1 e
+newton_cn :: (Float -> Float) -> (Float -> Float) -> Float -> Int -> Float -> (Float,Int)
+newton_cn f df xn n e
+     | xn - xn1 < e = (xn1,n)
+     | n < maxN     = newton_cn f df xn1 (n+1) e
+     | otherwise    = (xn,n)
     where xn1 = -(f xn)/(df xn) + xn
+
+newton :: (Float -> Float) -> (Float -> Float) -> Float -> Float -> Float
+newton f df xn e = m
+    where (m,_) = newton_cn f df xn 0 e
 ------------------------------------------
 
 -- Main ----------------------------------
 epsilons = map (\x -> pow 10 (-x)) [0..]
-prec = 6
+prec = 10
 p = 2 :: Int
 x = 2 :: Float
+maxN = 1000
 
 main = do putStrLn $ (++) "Precision : "   $ show $ epsilons !! prec
           putStrLn $ (++) "With secant : " $ show $ scs !! prec
@@ -53,8 +73,8 @@ main = do putStrLn $ (++) "Precision : "   $ show $ epsilons !! prec
           putStrLn $ (++) "With newton : " $ show $ nts !! prec
     where fn = (f p x)
           df = delta fn
-          scs = map (secant fn 0  x) epsilons
-          rts = map (root   fn 0  x) epsilons
-          nts = map (newton fn df x) epsilons
+          scs = map (secant_cn fn 0  x 0) epsilons
+          rts = map (root_cn   fn 0  x 0) epsilons
+          nts = map (newton_cn fn df x 0) epsilons
 ------------------------------------------
 
