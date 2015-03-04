eaffdd7bbd5255e245fc720a4307bc6e25cd5c3f
lucas8 <luc.linux@mailoo.org>
2015-03-04 09:22:56 +0100

    Improved conversion algorithm.

--- ipt/recursive/conv/conv.hs
+++ ipt/recursive/conv/conv.hs
@@ -4,15 +4,12 @@ horner [] _      = 1
 horner (a:[]) _  = a
 horner (a:b:l) p = horner ((a*p + b) : l) p
 
-join :: [String] -> String
-join [] = ""
-join (s:ss) = s ++ join ss
-
 topbase :: Int -> Int -> String
-topbase p n = join $ map show $ mtopbase p n []
+topbase p n = map (vals!!) $ mtopbase p n []
     where mtopbase _ 0 l = l
           mtopbase p n l = mtopbase p q (r:l)
               where (q,r) = divMod n p
+          vals = ['0'..'9'] ++ ['a'..'z'] ++ ['A'..'Z']
 
 t = [1, 0, 1, 0, 0] :: [Int]
 main = putStrLn $ show $ horner t 2
