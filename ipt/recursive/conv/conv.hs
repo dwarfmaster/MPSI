2320ec7a4fb37ed2e8e30463781ced09374a1cd7
lucas8 <luc.linux@mailoo.org>
2015-03-18 15:42:18 +0100

    Added conversion code in haskell.

--- ipt/recursive/conv/conv.hs
+++ ipt/recursive/conv/conv.hs
@@ -4,13 +4,23 @@ horner [] _      = 1
 horner (a:[]) _  = a
 horner (a:b:l) p = horner ((a*p + b) : l) p
 
+vals = ['0'..'9'] ++ ['a'..'z'] ++ ['A'..'Z']
+
 topbase :: Int -> Int -> String
 topbase p n = map (vals!!) $ mtopbase p n []
     where mtopbase _ 0 l = l
           mtopbase p n l = mtopbase p q (r:l)
               where (q,r) = divMod n p
-          vals = ['0'..'9'] ++ ['a'..'z'] ++ ['A'..'Z']
 
-t = [1, 0, 1, 0, 0] :: [Int]
-main = putStrLn $ show $ horner t 2
+approxp :: Int -> Double -> Double -> String
+approxp p x e
+ | x < 0     = '-' : approxp p (-1 * x) e
+ | x >= 1    = let (n,d) = properFraction x in topbase p n ++ approxp p d e
+ | otherwise = '.':mapproxp p x n []
+    where n = abs $ ceiling $ logBase (fromIntegral p) e
+          mapproxp :: Int -> Double -> Int -> String -> String
+          mapproxp _ _ 0 l = l
+          mapproxp p x n l = mapproxp p d (n - 1) (l ++ [vals !! i])
+              where nx = x * fromIntegral p
+                    (i,d) = properFraction nx
 
